a4826f96530bda3d125919b4f87f2738b8dc47aa
Mariawillow <mani0013@stud.kea.dk>
2024-06-10 21:58:52 +0200

    jstext

--- js/burger.js
+++ js/burger.js
@@ -1,22 +1,31 @@
 /* JavaScript der sætter klassen "active" på    */
 /* <div class="hamburger"> og                   */
 /* <ul class="nav-menu">                        */
-// Step 8
+// Der refereres til de HTML klasserne "hamburger og nav-menu".
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-
+// Event listener bliver tilføjet, så når der klikkes på "hamburger" kalder den på "mobilMenu".
 hamburger.addEventListener("click", mobileMenu);
 
+// "mobilMenu" defineres. 
+// Den fortæller at "hambuger" og "navMenu" er aktive. 
+// menuen vises.
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
 
-/* JavaScript der sørger for at burger menuen lukker når man klikker på et link*/
-// Step 9
+// Navigationlinks i HTML vælges og der tilføjes eventlistener til dem.
+/* JavaScript der sørger for at hvert (forEach) navLink - element tilføjes en 
+eventlistener som kalder på "closeMenu" når man klikker på et link*/
+// 
 const navLink = document.querySelectorAll(".nav-link");
 
+
+// Til sidst deffineres funktionen "closeMenu".
+// "closeMenu" fjerner (remove) "active" i begge elementer. 
+// Altså burgermenuen lukkes ned igen, når man klikker på et link.
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,23 @@
 
 // Når man klikker på "Projekt oversigt" knappen, skiftes der mellem at skjule go vise indholdet i rullemenu.
+// Funktionen finder id elementet "myDropdown" og toggler klassen "show". 
+// Altså tilføjer "show" hvis den ikke er tilstede, og fjerne hvis den er. 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
 //   Nedenfor vises hvordan man får rullemenuen væk igen, ved at klikke udenfor boksen
+// "window.onclick" lytter efter klik.
+// Funktionen lytter om det element ("event.target") hvor der klikkes, ikke har klassen "dropbtn"
+// Hvis der klikkes udenfor knappen, hentes alle elementer med klassen "dropdown-content".
+
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
+// "if" et element har klassen Show, fjernes den.
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
